4f7a716842a7c1b3b1e60e7584ec3fc6f6e992ac
Oghomwen Aigbedion <oghomwen.aigbedion@ISW-GSTAD-OA.Interswitchng.com>
2021-05-03 13:20:11 +0100

    Initial Commit (including datasource configuration)

---
+++ src/main/java/com/tbg/mars/MissionToMarsApplication.java
@@ -0,0 +1,13 @@
+package com.tbg.mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionToMarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionToMarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbg/mars/MissionToMarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbg.mars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionToMarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
